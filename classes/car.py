57f88acddf29d36beae39b1022ba0c992d5e3e4f
borodinsky <borodinsky.v@gmail.com>
2024-08-09 16:13:12 +0300

    ggg

--- classes/car.py
+++ classes/car.py
@@ -24,17 +24,31 @@ class Car:
     def increment_odometer(self, miles):
         self.odometer_reading += miles
     
+class ElectricCar(Car):
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+        self.battery_size = 40
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}- kWh battery.")
+
 my_new_car = Car('audi', 'a4', 2024)
 my_used_car = Car('subaru', 'outback', 2019)
+my_electric_car = ElectricCar('tesla', 'super model s', 2022)
 
 # print(my_new_car.get_descriptive_name())
-print(my_used_car.get_descriptive_name())
+# print(my_used_car.get_descriptive_name())
+# print(my_electric_car.get_descriptive_name())
 
 # my_new_car.update_odometer(15)
-my_used_car.update_odometer(23500)
+# my_used_car.update_odometer(23500)
 
 # my_new_car.read_odometer()
 
-my_used_car.read_odometer()
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+# my_used_car.read_odometer()
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
+
+my_electric_car.describe_battery()
